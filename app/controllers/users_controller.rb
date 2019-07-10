ae5788fca17b3df42c4e86a0aa1a3a344c4fc5f4
Katsutoshi_Hayashi <ezio1119@yahoo.co.jp>
2019-07-10 18:14:31 +0900

    All Fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,5 @@ class UsersController < ApplicationController
   def index
     added_users = User.where(id: params[:addedIDs])
     @users = User.where("name LIKE ?","#{params[:input]}%") - added_users
-
   end
-    
-
 end
